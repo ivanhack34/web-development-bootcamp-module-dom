43e474c6838ef811857e2044cdd648cb5ab03aa6
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-03-15 16:59:19 -0400

    Inicio del modulo
    Este es el inicio del modulo de dom

---
+++ DOM/Class-1/index.js
@@ -0,0 +1,12 @@
+//alert("Hello");
+
+document.querySelector("h1").innerHTML = "Good Bye";
+
+//?1-Respuesta a la clase 2 del modulo de DOM
+//document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Ivan";
+
+//busca el primer elemento secundario (child) del elemento raíz del documento HTML (document.firstElementChild),
+//luego busca el último elemento secundario del último elemento secundario del último elemento secundario de ese primer elemento secundario 
+//(es decir, el último elemento del último elemento del último elemento hijo del elemento raíz).
+//Finalmente, establece el contenido HTML (innerHTML) de ese elemento en la cadena "Angela".
+
